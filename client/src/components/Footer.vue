0c6ceb3736428a28355e00a003caaa78f20f2d5c
Junior Paz Formoso <junior.paz.formoso@gmail.com>
2021-05-16 14:59:14 -0400

    Added Footer component

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer app dark color="primary">
+    <v-container>
+      <v-layout>
+        <v-flex text-center>TMS. MADE WITH <v-icon color="red">mdi-heart</v-icon></v-flex>
+      </v-layout>
+    </v-container>
+  </v-footer>
+</template>
