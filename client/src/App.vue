17179d69861652b5cda2b76f80067dac417e9d43
Junior Paz Formoso <junior.paz.formoso@gmail.com>
2021-05-16 14:54:31 -0400

    Added mainly files for client app

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Layout>
+      <router-view />
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from "./components/Layout";
+
+export default {
+  components: {
+    Layout
+  }
+};
+</script>
