91a1f3d6281264446ff45c20a0a07093e0b48026
Junior Paz Formoso <junior.paz.formoso@gmail.com>
2021-05-18 16:21:25 -0400

    Added config files to read enviroment

---
+++ server/test/config.js
@@ -0,0 +1,9 @@
+export const elastic = {
+    url: "https://bd7a3bbd2e184113af21c4cc01be9722.us-central1.gcp.cloud.es.io:9243",
+    user: "elastic",
+    password: "csKAR2JMJiks9GJ5IrdvmqtB"
+}
+
+export const mongo = {
+    url: "mongodb+srv://admin:zaq1ZAQ!@cluster0-yhumi.gcp.mongodb.net/test?retryWrites=true&w=majority"
+}
